29c95dadeeac5744455fc13b5b6eb8c207bc4238
Vishaka <Vishaka7721@gmail.com>
2024-04-20 11:31:23 +0530

    Update files

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Intro from './Components/Introduction/Intro';
 import Navbar from './Components/Navbar/Navbar';
 import Carousel from './Components/Slider/Carousel';
 import Testimonial from './Components/Testimonial/Testimonial';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Specialities from './Pages/Specialities/Specialities';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Intro />
-      <Carousel />
-      <Testimonial />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/specialities" component={Specialities} />
+        </Routes>
+        <Intro />
+        <Carousel />
+        <Testimonial />
+        <Footer />
+      </Router>
     </div>
   );
 }
